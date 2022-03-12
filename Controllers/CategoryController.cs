82dba93fa63210c07cf04b56b74e74880cef4098
adel abd ulhakeem <adel20142658@gmail.com>
2022-03-12 23:03:30 +0200

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy_Management.Models;
+using Pharmacy_Management.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository categoryRepository { get; set; }
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Category> categories = categoryRepository.GetCategories();
+            return Ok(categories);
+        }
+    }
+}
---
+++ Models/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Management.Models.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+            Category category = new Category() { CategoryId = 1, CategoryName = "Alphapitc" };
+            categories.Add(category);
+
+            category = new Category() { CategoryId = 2, CategoryName = "Parstmoal" };
+            categories.Add(category);
+
+            category = new Category() { CategoryId = 3, CategoryName = "Antibayotic" };
+            categories.Add(category);
+
+            return categories;
+        }
+    }
+}
